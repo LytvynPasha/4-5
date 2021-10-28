a44ca43ea014ba0cea5cbb0392c2e717b22431c4
blck f0x <lytvyn.pavlo@gmail.com>
2021-10-28 09:56:27 +0300

    Добавьте файлы проекта.

---
+++ 4,5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		if ((y <= -sqrt(pow(R * 1, 2) - pow(x, 2)) && y != -sqrt(pow(R * 2, 2) - pow(x, 2)) && x >= 0 && y >= 0)
+			|| (y <= -sqrt(pow(R * 1, 2) - pow(x, 2) && y) != -sqrt(pow(R * 2, 2) - pow(x, 2)) && x <= 0 && y <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+		for (int i = 0; i < 10; i++)
+		{
+			x = 6. * rand() / RAND_MAX - 3;
+			y = 6. * rand() / RAND_MAX - 3;
+			if ((y <= -sqrt(pow(R * 1, 2) - pow(x, 2)) && y != -sqrt(pow(R * 2, 2) - pow(x, 2)) && x >= 0 && y >= 0)
+				|| (y <= -sqrt(pow(R * 1, 2) - pow(x, 2) && y) != -sqrt(pow(R * 2, 2) - pow(x, 2)) && x <= 0 && y <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
